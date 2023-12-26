52dcdfa85578d61a5d4f4e5d7f9034123002d643
Cristina Correa <cristina.correa.segade@gmail.com>
2023-12-26 02:59:55 +0100

    second feature added

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-function toggle() {
-  const TOGGLE_ICON = document.querySelector(".ico01");
+function toggleFirst() {
+  const TOGGLE_ICON_GROUP1 = document.getElementById("icon1-group1");
 
-  TOGGLE_ICON.addEventListener("mouseover", active, false);
+  TOGGLE_ICON_GROUP1.addEventListener("mouseover", activeFirst, false);
 
-  function active() {
-    const WRAPPER = document.querySelector(".wrapper");
+  function activeFirst() {
+    const WRAPPER = document.querySelector(".feature1");
 
     WRAPPER.classList.toggle("active");
+    document.getElementById("ul1").classList.toggle("active");
   }
 }
 
-toggle();
+function toggleSecond() {
+  const TOGGLE_ICON_GROUP2 = document.getElementById("icon1-group2");
+
+  TOGGLE_ICON_GROUP2.addEventListener("mouseover", activeSecond, false);
+
+  function activeSecond() {
+    const WRAPPER = document.querySelector(".feature2");
+
+    WRAPPER.classList.toggle("active");
+    document.getElementById("ul2").classList.toggle("active");
+  }
+}
+
+toggleFirst();
+
+toggleSecond();
